4db6ed2a9bb88df3832b532c7ff8c64a0056c9ca
Ina Botnaru <ibotnaru@e1z2r7p29.42.us.org>
2019-05-16 15:57:34 -0700

    success

---
+++ level04/ft_list_foreach/ft_list_foreach.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_foreach.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ibotnaru <ibotnaru@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/16 15:56:33 by ibotnaru          #+#    #+#             */
+/*   Updated: 2019/05/16 15:56:49 by ibotnaru         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+void	ft_list_foreach(t_list *begin_list, void (*f)(void *))
+{
+	while (begin_list)
+	{
+		f(begin_list->data);
+		begin_list = begin_list->next;
+	}
+}
